abe2ad713e1d8c5a1b9514ec2b1dd8f0c82c3915
三 苏 <windy3417@LIVE.COM>
2019-11-13 11:21:57 +0800

    Auto open download file

--- 成本管理/UI/FormPOContractList.cs
+++ 成本管理/UI/FormPOContractList.cs
@@ -21,19 +21,22 @@ namespace 成本管理.UI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string sql= "select b.FileContent from manage..ximai_v_SOattachment as a inner join ufdata_017_2017..so_somain as c  on a.销售订单号 = c.csocode inner join UFDATA_017_2017..VoucherAccessories as b  on c.id = b.VoucherID";
+            string sql= "select b.[filename] , b.FileContent from manage..ximai_v_SOattachment as a inner join ufdata_017_2017..so_somain as c  on a.销售订单号 = c.csocode inner join UFDATA_017_2017..VoucherAccessories as b  on c.id = b.VoucherID";
             SqlDataReader rd= sqlHelper.GetSqlDataReader(sql);
             rd.Read();
+            string contractName = (string)rd["Filename"];
             byte[] contractFiles = (byte[]) rd["FileContent"];
+            rd.Close();
             int filelength = contractFiles.Length;
-            string contractName = "1.pdf";
+            
             string myUrl = Environment.CurrentDirectory + "\\" + contractName;
             FileStream fs = new FileStream(myUrl, FileMode.OpenOrCreate, FileAccess.Write);
             BinaryWriter BW = new BinaryWriter(fs);
             BW.BaseStream.Write(contractFiles, 0, filelength);
             BW.Flush();
             BW.Close();
-            MessageBox.Show("文化读取成功");
+            MessageBox.Show("文件读取成功");
+            System.Diagnostics.Process.Start(myUrl);
         }
     }
 }
